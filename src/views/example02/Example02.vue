ba4254bb7a439bef046f42650fb918117f76f11b
Gao Ruijie <vipgrjyx@163.com>
2020-04-15 23:29:12 +0800

    computed 路由传参

---
+++ src/views/example02/Example02.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <p>{{ user.name }} / {{ replace }}</p>
+    <p>
+      计算属性，默认声明的是属性的getter方法，不支持传参。但JS中，属性可以是一个，函数
+    </p>
+    <p>{{ myReplace(user.insertTime) }}</p>
+    <button @click="change">click</button>
+  </div>
+</template>
+<script>
+export default {
+  //   data() {
+  //     return {
+  //       msg: "df",
+  //       user: {
+  //         name: "JIE"
+  //       },
+  //       courses: []
+  //     };
+
+  data: () => ({
+    msg: "df",
+    user: {
+      name: "JIE",
+      insertTime: "2020.04.09T08:30:21"
+    },
+    courses: []
+  }),
+
+  methods: {
+    change() {
+      this.user.name = "SUN";
+    }
+  },
+
+  computed: {
+    replace() {
+      return this.user.insertTime.replace("T", " ");
+    },
+    myReplace() {
+      return data => data.replace("T", " ");
+    }
+  }
+};
+</script>
---
+++ src/views/example03/Example03.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p>在router启用props支持。在vue props属性中声明同名变量</p>
+    studentID: {{ sid }}
+    <br />
+    homeworksID: {{ hid }}
+    <br />
+    也可通过$route.params调用
+    <br />
+    {{ $route.params.sid }}
+    <hr />
+    {{ getTitle }}
+  </div>
+</template>
+<script>
+export default {
+  props: ["sid", "hid"],
+  data: () => ({
+    homeworks: [
+      { id: 1, title: "spring" },
+      { id: 2, title: "springMVC" },
+      { id: 3, title: "JPA" }
+    ]
+  }),
+  created() {
+    console.log("created");
+  },
+  // 第一次不会改变，数据改变后，要重新渲染时候会调用此方法
+  beforeUpdate() {
+    console.log("beforeupdate()");
+  },
+  // beforeRouteUpdate(to, from, next) {
+  //   console.log("beforeRouterUpdate");
+  //   // 不执行next方法，不会继续往下执行
+  //   next();
+  // },
+  // beforeUpdate() {
+  //   console.log("beforeUpdate()");
+  // },
+  // beforeRouteUpdate(to, from, next) {
+  //   console.log("beforeRouterUpdate()");
+  //   next();
+  // },
+  computed: {
+    getTitle() {
+      return this.homeworks.find(h => h.id == this.hid).title;
+    }
+  }
+};
+</script>
